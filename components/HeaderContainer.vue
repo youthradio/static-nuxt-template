f1e733ba60ce0794508b1797dac8fe2e64a9dc45
Radames Ajna <radamajna@gmail.com>
2019-10-09 12:35:45 -0700

    moving header to container
    cleaning up css for template

---
+++ components/HeaderContainer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <FeatureHeader
+      :header-data="headerData"
+      @onHeaderImgHeight="setHeaderHeight"
+    />
+    <MenuHeader :offset="headerImageHeight" />
+  </div>
+</template>
+
+<script>
+import POSTCONFIG from '../post.config'
+import MenuHeader from '~/components/MenuHeader'
+import FeatureHeader from '~/components/FeatureHeader'
+
+export default {
+  name: 'HeadContainer',
+  components: {
+    FeatureHeader,
+    MenuHeader
+  },
+  data () {
+    return {
+      headerImageHeight: 0
+    }
+  },
+  computed: {
+    headerData () {
+      return {
+        featureImage: POSTCONFIG.featureImagePath,
+        title: POSTCONFIG.title,
+        author: POSTCONFIG.author,
+        imageCaption: POSTCONFIG.featureImageCaption,
+        publishDate: POSTCONFIG.publishDate,
+        location: POSTCONFIG.location
+      }
+    }
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+    setHeaderHeight (val) {
+      this.headerImageHeight = val
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import '~@/css/_vars';
+
+</style>
